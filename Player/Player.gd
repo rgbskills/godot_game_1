5ad563f5fbca080d308755ebf8389a556ec2ae1f
Zoltan Benko <rgbskills@gmail.com>
2020-03-18 11:39:08 +0200

    add spikes

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,8 @@ const UP = Vector2(0, -1)
 const JUMP_SPEED = 4200
 const WORLD_LIMIT = 4000
 
+var lives = 3
+
 signal enimate
 
 func _physics_process(delta):
@@ -38,9 +40,20 @@ func move():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
+		$JumpSFX.play()
 	
 func animate():
 	emit_signal("enimate", motion)
 	
 func end_game():
 	get_tree().change_scene("res://levels/GameOver.tscn")
+
+func hurt():
+	position.y -= 1
+	# Yeald - Dont do anyithing until I tell you otherwise
+	yield(get_tree(), "idle_frame") # Move up by a pixe; Wait one frame; This will bypass gravity
+	motion.y -= JUMP_SPEED
+	lives -= 1
+	$HurtSFX.play()
+	if lives < 0:
+		end_game()
