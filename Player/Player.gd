2ea82f2d0df7927233f51bb57d0952866e23af85
Zoltan Benko <zoltan.benko@yopeso.com>
2020-05-06 16:37:23 +0300

    added gui

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,8 +9,6 @@ const JUMP_SPEED = 4200
 const WORLD_LIMIT = 4000
 const BOOST_MULTIPLIER = 1.5
 
-var lives = 3
-
 signal enimate
 
 func _physics_process(delta):
@@ -22,7 +20,7 @@ func _physics_process(delta):
 	
 func apply_gravity():
 	if position.y > WORLD_LIMIT:
-		end_game()
+		get_tree().call_group("Gamestate", "end_game")
 	if is_on_floor():
 		motion.y = 0
 	elif is_on_ceiling():
@@ -45,19 +43,13 @@ func jump():
 	
 func animate():
 	emit_signal("enimate", motion)
-	
-func end_game():
-	get_tree().change_scene("res://levels/GameOver.tscn")
 
 func hurt():
 	position.y -= 1
 	# Yeald - Dont do anyithing until I tell you otherwise
 	yield(get_tree(), "idle_frame") # Move up by a pixe; Wait one frame; This will bypass gravity
 	motion.y -= JUMP_SPEED
-	lives -= 1
 	$HurtSFX.play()
-	if lives < 0:
-		end_game()
 
 func boost():
 	$JumpSFX.play()
